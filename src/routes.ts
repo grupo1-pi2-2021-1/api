f16f47452a3d79344cde08ce562a4d6d4cbf879c
Rafael Makaha <rafael.makaha@gmail.com>
2021-11-10 10:59:42 -0300

    fix: Add try catch statement on some routes
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>
    Co-authored-by: jpmartins201 <jpmartins201@live.com>

--- src/routes.ts
+++ src/routes.ts
@@ -10,14 +10,19 @@ routes.get("/", async(request, response) => {
 })
 
 routes.post("/user", async (request, response) => {
-  const { name, phone } = request.body;
-
-  await knex("users").insert({ name, phone }).returning('id')
-  .into('users')
-  .then(function (id) {
-    // use id here
-    return response.json({ id });
-  });
+  try {
+    const { name, phone } = request.body;
+  
+    await knex("users").insert({ name, phone }).returning('id')
+    .into('users')
+    .then(function (id) {
+      // use id here
+      return response.json({ id });
+    });
+  } catch (error) {
+    console.log(error)
+    return response.json(error)
+  }
 });
 
 routes.get("/ambulances", async (request, response) => {
@@ -69,38 +74,50 @@ routes.get("/proceduresSteps", async (request, response) => {
 });
 
 routes.post("/addProcedureHistory", async (request, response) => {
-  const { user_id, procedure_id } = request.body;
-  const hour = new Date();
-
-  await knex("history_procedure").insert({ user_id, procedure_id, hour });
-
-  return response.json({ sucess: "Deu certo" });
+  try {
+    const { user_id, procedure_id } = request.body;
+    const hour = new Date();
+  
+    await knex("history_procedure").insert({ user_id, procedure_id, hour });
+  
+    return response.json({ sucess: "Deu certo" });
+    
+  } catch (error) {
+    console.log(error)
+    return response.json(error)
+  }
 });
 
 routes.get("/:id/listProcedures", async (request, response) => {
-  const { id } = request.params;
-
-  const posts = await knex("history_procedure")
-    .leftJoin("users", "users.id", "history_procedure.user_id")
-    .leftJoin("procedure", "procedure.id", "history_procedure.procedure_id")
-    .select(
-      "history_procedure.id",
-      "users.name",
-      "procedure.title",
-      "history_procedure.hour"
-    )
-    .where({ user_id: id });
-
-  const serializedItems = posts.map((item) => {
-    return {
-      name: item.name,
-      title: item.title,
-      hour: new Date(item.hour).toLocaleString("pt-BR"),
-      id: item.id
-    };
-  });
-
-  return response.json(serializedItems);
+  try {
+    
+    const { id } = request.params;
+  
+    const posts = await knex("history_procedure")
+      .leftJoin("users", "users.id", "history_procedure.user_id")
+      .leftJoin("procedure", "procedure.id", "history_procedure.procedure_id")
+      .select(
+        "history_procedure.id",
+        "users.name",
+        "procedure.title",
+        "history_procedure.hour"
+      )
+      .where({ user_id: id });
+  
+    const serializedItems = posts.map((item) => {
+      return {
+        name: item.name,
+        title: item.title,
+        hour: new Date(item.hour).toLocaleString("pt-BR"),
+        id: item.id
+      };
+    });
+  
+    return response.json(serializedItems);
+  } catch (error) {
+    console.log(error)
+    return response.json(error)
+  }
 });
 
 /*routes.get("/:id/steps", async (request, response) => {
